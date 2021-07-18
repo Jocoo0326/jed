a14d436bf42907aa3b44bb579ae35cd1a1bcf507
jocoo <xzpnuist@gmail.com>
2021-07-19 00:13:44 +0800

    update

---
+++ gl_extra.h
@@ -0,0 +1,18 @@
+#ifndef GL_EXTRA_H
+#define GL_EXTRA_H
+
+#define GL_GLEXT_PROTOTYPES
+#include <SDL2/SDL_opengl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "file.h"
+
+bool compile_shader_source(const GLchar *source, GLenum shader_type,
+                           GLuint *shader);
+bool compile_shader_file(const char *file_path, GLenum shader_type,
+                         GLuint *shader);
+bool link_program(GLuint vert_shader, GLuint frag_shader, GLuint *program);
+
+#endif /* GL_EXTRA_H */
--- la.c
+++ la.c
@@ -9,13 +9,43 @@ Vec2f vec2f_add(Vec2f a, Vec2f b) {
 }
 
 Vec2f vec2f_sub(Vec2f a, Vec2f b) {
-  return (Vec2f){.x = a.x + b.x, .y = a.y + b.y};
+  return (Vec2f){.x = a.x - b.x, .y = a.y - b.y};
 }
 
 Vec2f vec2f_mul(Vec2f a, Vec2f b) {
-  return (Vec2f){.x = a.x + b.x, .y = a.y + b.y};
+  return (Vec2f){.x = a.x * b.x, .y = a.y * b.y};
+}
+
+Vec2f vec2f_mul3(Vec2f a, Vec2f b, Vec2f c) {
+  return vec2f_mul(vec2f_mul(a, b), c);
 }
 
 Vec2f vec2f_div(Vec2f a, Vec2f b) {
-  return (Vec2f){.x = a.x + b.x, .y = a.y + b.y};
+  return (Vec2f){.x = a.x / b.x, .y = a.y / b.y};
+}
+
+Vec4f vec4f(float x, float y, float z, float w) {
+  return (Vec4f){.x = x, .y = y, .z = z, .w = w};
+}
+
+Vec4f vec4fs(float x) { return (Vec4f){.x = x, .y = x, .z = x, .w = x}; }
+
+Vec4f vec4f_add(Vec4f a, Vec4f b) {
+  return (Vec4f){
+      .x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z, .w = a.w + b.w};
+}
+
+Vec4f vec4f_sub(Vec4f a, Vec4f b) {
+  return (Vec4f){
+      .x = a.x - b.x, .y = a.y - b.y, .z = a.z - b.z, .w = a.w - b.w};
+}
+
+Vec4f vec4f_mul(Vec4f a, Vec4f b) {
+  return (Vec4f){
+      .x = a.x * b.x, .y = a.y * b.y, .z = a.z * b.z, .w = a.w * b.w};
+}
+
+Vec4f vec4f_div(Vec4f a, Vec4f b) {
+  return (Vec4f){
+      .x = a.x / b.x, .y = a.y / b.y, .z = a.z / b.z, .w = a.w / b.w};
 }
